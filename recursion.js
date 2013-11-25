2b77be7b70f24213dfc377b0bc0c2d1178475d39
Suvankar Satpati <suvankars@rssoftware.co.in>
2013-11-25 19:27:55 +0530

    Prevent mutation of bands array

--- recursion.js
+++ recursion.js
@@ -3,18 +3,20 @@
 //Recursively iterating over an array using higher order function
 //Created a variable called 'each' and store a function
 
+var bands = ["Coldplay", "The Beatles", "Metallica","Pink Floyd", "Linkin Park", "The Doors"];
+n  = bands.length;
+
 var each = function(arr, foo){
 
-	if (arr.length==0){
+	if (n==0){
 		return false; // stop further execution
 	}
-
-	foo(arr[0]);
-	arr.shift();
+	foo(arr[bands.length-n]);
+	n -= 1
 	each(arr, foo);  
 };	
 
-var bands = ["Coldplay", "The Beatles", "Metallica","Pink Floyd", "Linkin Park", "The Doors"];
 each(bands, function (band_name){
 	console.log(band_name);
 });
+
