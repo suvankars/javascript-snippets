c30cbf6cda81ed2866b4abc4f03fe2d5eed1961c
Suvankar Satpati <suvankars@rssoftware.co.in>
2013-11-27 11:51:48 +0530

    implemented dropWhile

--- array-functions.js
+++ array-functions.js
@@ -35,9 +35,9 @@ a new array which contains the first occurrence of every element of this array.
 var distinct = function(a) {
     unique = [];
     for (var i in a ) {
-	if (unique.indexOf(a[i]) == -1){
-	    unique.push(a[i])
-	}
+		if (unique.indexOf(a[i]) == -1){
+			unique.push(a[i])
+		}
     }	
 };
 
@@ -77,10 +77,71 @@ Returns:
 
 var dropRight = function(a, n){
     function rightIndex(n){
-	return a.length-n;
+		return a.length-n;
     }
-    a.splice(rightIndex(n), n) //should return the array [1, 2, 3].
-};
+
+    a.splice(rightIndex(n), n)
+	return a;
+}; //should return the array [1, 2, 3]
 
 dropRight([1, 2, 3, 4, 5], 2)
 
+/*	
+	dropWhile
+	.........
+
+	Drops longest prefix of elements that satisfy a predicate.
+		a is an array.
+		p is a predicate.
+	Returns:
+		the longest suffix of this array whose first element does not satisfy the predicate p.
+*/
+
+function isOdd(e){
+	return (e%2) !=0;
+};
+
+
+var dropWhile = function(a,p){
+	for(var i in a){
+		if (isOdd(a[i])){
+			a.splice(i, 1);
+		}
+	}
+	return a;
+};	
+
+dropWhile([1,2,3,4,5], isOdd);	//should return the array [2, 4]
+
+
+
+
+/*
+ endsWith
+ ........
+
+ Tests whether this array ends with the given sequence.
+	 a is an array.
+	 b the sequence to test.
+ Returns:
+	 true if this array has b as a suffix, false otherwise.
+*/
+
+function endsWith(a,b){
+	foundMatch = true;
+	if (a.length >= b.length) {
+		startIndex = a.length - b.length
+	} else {
+		return !foundMatch;
+	}
+	
+	for (i in b){
+		if (a[(startIndex+parseInt(i))] != b[i]){
+			foundMatch = false;
+			return foundMatch;
+		}
+	}
+	return foundMatch;
+}
+
+(endsWith([1,2,3,4,5], [23,4,5]) // should retun true;
