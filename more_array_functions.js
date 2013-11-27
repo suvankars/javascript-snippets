439a141693e42496532a87909e56387a8f1b81d4
suvankar <suvankar.17@gmail.com>
2013-11-27 08:23:07 +0530

    Finds the first element of the array satisfying a predicate, if any

--- more_array_functions.js
+++ more_array_functions.js
@@ -11,18 +11,18 @@ Returns:
     a new array consisting of all elements of this array that satisfy the given predicate p. The order of the elements is preserved.
 */
 
-function filterElement(a,p){
-    var filteredElement = a.filter(isEven);
-    return filteredElement; //should return the array [2, 4, 6, 8, 10]
+    function filterElement(a,p){
+	var filteredElement = a.filter(isEven);
+	return filteredElement; //should return the array [2, 4, 6, 8, 10]
 
-}
+    }
 
 
-var isEven = function (e){
-    return e%2 == 0;
-}
+    var isEven = function (e){
+	return e%2 == 0;
+    }
 
-console.log (filterElement([1,2,3,4,5,6,7,8,9,10], isEven))
+    filterElement([1,2,3,4,5,6,7,8,9,10], isEven)
 
 /*
 filterNot
@@ -34,13 +34,41 @@ Returns:
     a new array consisting of all elements of this array that do not satisfy the given predicate p. The order of the elements is preserved.
 */
 
-function filterNot(a,p){
-    var filteredElement = a.filter(isEven)
-    return filteredElement; //should return the array [1, 3, 5, 7, 9]
-}
+    function filterNot(a,p){
+	var filteredElement = a.filter(isEven)
+	return filteredElement; //should return the array [1, 3, 5, 7, 9]
+    }
 
-var isEven = function(e){
-    return e%2 != 0;
-}
+    var isEven = function(e){
+	return e%2 != 0;
+    }
 
-console.log (filterNot([1,2,3,4,5,6,7,8,9,10], isEven))
+    filterNot([1,2,3,4,5,6,7,8,9,10], isEven)
+
+/*
+find
+....
+
+Finds the first element of the array satisfying a predicate, if any.
+    a is an array.
+    p is the predicate used to test elements.
+Returns:
+    An option value containing the first element in the array that satisfies p, or None if none exists.
+*/
+
+
+    function find(a,p){
+	var matchFound = 'None';
+	for (i in a ){
+	    if (isEven(a[i])){
+		matchFound = a[i];
+		break;
+	    }
+	}
+	return matchFound; //should return 2
+    }
+    var isEven = function (e){
+	return e%2==0;
+    }
+
+find([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], isEven)
